a2a0c745594f561fb3192810ec5c7d1beddc3357
Ramy Magdy <ramy.comm@gmail.com>
2025-05-20 15:32:50 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from egxpy import stock_data
+
+app = FastAPI()
+
+@app.get("/price/{ticker}")
+def get_price(ticker: str):
+    try:
+        data = stock_data.get_stock_summary(ticker)
+        return {"success": True, "data": data}
+    except Exception as e:
+        return {"success": False, "error": str(e)}
