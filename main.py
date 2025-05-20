ad74c2164c4d7bbf531935a2383b064d5d4ae7bb
Ramy Magdy <ramy.comm@gmail.com>
2025-05-20 16:40:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,54 @@
 from fastapi import FastAPI
-from egxpy.download import get_EGX_intraday_data
-from datetime import datetime
+from datetime import datetime, date
+import pandas as pd
+from tvDatafeed import TvDatafeedLive, Interval
+from retry import retry
 
 app = FastAPI()
 
-@app.get("/price/{ticker}")
-def get_price(ticker: str):
-    try:
-        interval = "15min"
-        today = datetime.today().strftime("%Y-%m-%d")
-        
-        data = get_EGX_intraday_data(ticker=ticker, interval=interval, start=today, end=today)
+@retry((Exception), tries=20, delay=0.5, backoff=0)
+def _get_intraday_close_price_data(symbol, exchange, interval, n_bars):
+    interval_dic = {
+        '1 Minute': Interval.in_1_minute,
+        '5 Minute': Interval.in_5_minute,
+        '30 Minute': Interval.in_30_minute
+    }
 
-        # Convert to list of dicts for JSON response if data is a DataFrame
-        return {"success": True, "data": data.to_dict(orient="records")}
+    tv = TvDatafeedLive()
+    response = tv.get_hist(
+        symbol=symbol,
+        exchange=exchange,
+        interval=interval_dic[interval],
+        n_bars=n_bars,
+        timeout=-1
+    )['close']
+    return response
+
+
+@app.get("/intraday/{ticker}")
+def get_intraday_price(
+    ticker: str,
+    interval: str = "5 Minute",
+    n_bars: int = 50
+):
+    try:
+        close_data = _get_intraday_close_price_data(
+            symbol=ticker,
+            exchange="EGX",
+            interval=interval,
+            n_bars=n_bars
+        )
+
+        df = close_data.reset_index()
+        df.columns = ["datetime", "close"]
+        df["datetime"] = df["datetime"].astype(str)
+        return {
+            "success": True,
+            "ticker": ticker,
+            "interval": interval,
+            "n_bars": n_bars,
+            "data": df.to_dict(orient="records")
+        }
+
     except Exception as e:
         return {"success": False, "error": str(e)}
